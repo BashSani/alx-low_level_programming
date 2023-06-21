05bbc66e0f5a8f56580a2d707cbb051c6e7a6408
BashSani <120298336+BashSani@users.noreply.github.com>
2023-06-21 22:05:05 +0000

    Size of Data type

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,20 +2,14 @@
 /**
  * main - program to print size of data type
  * Description: size of diff. datatype
- * Return: 0(Success)
+ * Return: 0 (Success)
  */
 int main(void)
 {
-	char a;
-	int b;
-	long int c;
-	long long int d;
-	float f;
-	
-	printf("Size of a char: %u byte(s)\n", sizeof(a));
-	printf("Size of an int: %u byte(s)\n", sizeof(b));
-	printf("Size of a long int: %u byte(s)\n", sizeof(c));
-	printf("Size of a long long int: %u byte(s)\n", (sizeof(d));
-	printf("Size of a float: %u byte(s)", (sizeof(f));
+	printf("Size of a char: %u byte(s)\n", sizeof(char));
+	printf("Size of an int: %u byte(s)\n", sizeof(int));
+	printf("Size of a long int: %u byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %u byte(s)\n", (sizeof(long long int));
+	printf("Size of a float: %u byte(s)", (sizeof(float));
 	return (0);
-}
+
