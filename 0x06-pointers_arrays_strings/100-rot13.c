9f882c66cda0b6a2be66ea1eade8ac70957d41d3
BashSani <bashirsani1ille17@gmail.com>
2023-07-01 02:47:13 -0700

    rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * rot13 - encode a strings
+ * @n: pointer to string
+ * Return: (n)
+ */
+char *rot13(char *n)
+{
+	int x, y;
+	char input1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char input2[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (x = 0; s[x] != '\0'; x++)
+	{
+		for (y = 0; y < 52; y++)
+		{
+			if (n[x] == input1[y])
+			{
+				n[x] = input[y];
+				break;
+			}
+		}
+	}
+	return (n);
+}
