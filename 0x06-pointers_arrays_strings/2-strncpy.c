8269c99a96988beb09e35f099a5eb799c4f4595b
BashSani <bashirsani1ille17@gmail.com>
2023-06-30 23:45:45 -0700

    strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -5,13 +5,13 @@
  * @src: copyiing file
  * @n: character to copy
  * Return: dest
- */
+*/
 char *_strncpy(char *dest, char *src, int n)
 {
 	int b;
 
 	b = 0;
-	while (b < n && src[b] != '\n')
+	while (b < n && src[b] != '\0')
 	{
 		dest[b] = src[b];
 		b++;
