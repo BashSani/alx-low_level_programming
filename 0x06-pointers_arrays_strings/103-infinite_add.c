80c738d5283f9b3040da49f7907393e3770738b2
BashSani <bashirsani1ille17@gmail.com>
2023-07-01 09:12:59 -0700

    infinite_add

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -27,5 +27,24 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		a--;
 		b--;
 		if (a >= 0)
+			e = n1[a] - '0';
+		else
+			e = 0;
+		if (b >= 0)
+			f = n2[b] - '0';
+		else
+			f = 0;
+		r[c] = (e + f + g) % 10 + '0';
+		g = (e + f + g) / 10;
 	}
+	if (g == 1)
+	{
+		r[d + 1] = '\0';
+		if (d + 2 > size_r)
+			return (0);
+		while (d-- >= 0)
+			r[d + 1] = r[d];
+		r[0] = g + '0';
+	}
+	return (r);
 }
