6ec014bf6964bc0cf47204d320aeb6dd393e1923
BashSani <bashirsani1ille17@gmail.com>
2023-07-02 03:21:04 -0700

    print_buffer

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -11,6 +11,7 @@ void print_buffer(char *b, int size)
 	int x, y, z, w;
 
 	x = 0;
+
 	if (size <= 0)
 	{
 		printf("\n");
@@ -23,7 +24,7 @@ void print_buffer(char *b, int size)
 		for (z = 0; z < 10; z++)
 		{
 			if (z < y)
-				printf("%02x: ", *(b + x + z));
+				printf("%02x", *(b + x + z));
 			else
 				printf(" ");
 			if (z % 2)
