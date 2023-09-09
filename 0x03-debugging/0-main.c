8f28e81d4bb6eab33f4ef322249fff69bc0a9d01
BashSani <bashirsani1ille17@gmail.com>
2023-09-09 12:06:34 -0700

    PosNeg

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,14 +1,13 @@
 #include "main.h"
-
 /**
- * main - test that the function
- * Return: Always(0)
- */
-
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
 int main(void)
 {
-	int a = 0;
+	int i;
 
-	positive_or_negative(a);
+	i = 98 % 2;
+	positive_or_negative(i);
 	return (0);
 }
