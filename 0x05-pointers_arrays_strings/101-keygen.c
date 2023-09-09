b6890461c66979021e2754539e25c7943da590af
BashSani <bashirsani1ille17@gmail.com>
2023-09-09 10:45:16 -0700

    Crackme

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - generates random valid passwords for the program
+ * Return: Always(0)
+ */
+
+int main(void)
+{
+	int a;
+	char b;
+
+	srand(time(NULL));
+	while (a <= 2645)
+	{
+		b = rand() % 128;
+		a += b;
+		putchar(b);
+	}
+	putchar(2772 - a);
+	return (0);
+}
